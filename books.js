21cdc6ba0ae69e094524da307ad1c85fa7a3df26
AOMuiz <abdulwasiumuiz@gmail.com>
2023-01-29 19:59:42 +0100

    add more books and images for some

--- books.js
+++ books.js
@@ -20,7 +20,7 @@ export const MUTOONS = [
     author: "Imam Barbahaaree",
     image: require("./assets/sharhsunnah.png"),
     summary:
-      "The author of this book is al-Barbahāree, and his name is: al-Hasan bin ‛Alī bin Khalaf al-Barbahāree, this title being an affiliation to “Barbahār” which is a type of medicine[1], so perhaps he used to work in the field of medicines or sell it. He was from the major Hanbalee scholars taking knowledge from those who studied under Imām Ahmad bin Hanbal, such as al-Marroozee[2] and others. He studied thoroughly, and delved deeply into knowledge. He acquired the ‛aqīdah, fiqh and other knowledge from the great scholars[3]. The title of the book is Sharh as-Sunnah, i.e. Explanation of the Sunnah – the intent of the term Sunnah here is The Path of the Messenger. The intent here is not that of the terminology employed by the Scholars of Hadīth who state: “The Sunnah is whatever is affirmed upon the Prophet of statements, actions or tacit approvals,” rather the intent is more general, that being: The Path of the Messenger, The Path of his Companions and the Path of the Pious Predecessors (as-Salaf as-Sālih) – and this is the Sunnah that has been transmitted and passed down, whether it be in belief, in worship, in fiqh, or in manners and ettiquette. All of this is referred to as the Sunnah in a general sense.",
+      "The author of this book is al-Barbahāree, and his name is: al-Hasan bin ‛Alī bin Khalaf al-Barbahāree, this title being an affiliation to “Barbahār” which is a type of medicine[1], so perhaps he used to work in the field of medicines or sell it. He was from the major Hanbalee scholars taking knowledge from those who studied under Imām Ahmad bin Hanbal, such as al-Marroozee[2] and others. He studied thoroughly, and delved deeply into knowledge. He acquired the ‛aqīdah, fiqh and other knowledge from the great scholars[3].\n The title of the book is Sharh as-Sunnah, i.e. Explanation of the Sunnah – the intent of the term Sunnah here is The Path of the Messenger. The intent here is not that of the terminology employed by the Scholars of Hadīth who state: “The Sunnah is whatever is affirmed upon the Prophet of statements, actions or tacit approvals,” rather the intent is more general, that being: The Path of the Messenger, The Path of his Companions and the Path of the Pious Predecessors (as-Salaf as-Sālih) – and this is the Sunnah that has been transmitted and passed down, whether it be in belief, in worship, in fiqh, or in manners and ettiquette. All of this is referred to as the Sunnah in a general sense.",
     bookUri:
       "https://abdurrahmanorg.files.wordpress.com/2014/09/sharh-assunnah-al-barbahary-arabic-matn.pdf",
     audio: {
@@ -48,9 +48,9 @@ export const MUTOONS = [
     id: "58694a0f-3da1-471f-bd96-145571e29d73",
     title: "Usool Thalatha",
     author: "Muhammad bn Abdulwahab",
-    image: require("./assets/usuulsunnah.png"),
+    image: require("./assets/three-principle.png"),
     summary:
-      "Thalaathatul-Usool (The Three Fundamental Principles) is one of the famous works of al Imaam Muhammad bin ‘Abdil-Wahhaab (may Allah have mercy on him). Without doubt The three fundamental principles of islam are amongst the most important matters that every person will be asked about; ‘Who is your Lord?’, ‘What is your religion?’ and ‘Who is your Prophet?’. So its obligatory upon mankind to know its proofs and making it a study until we reach our next abode, the Grave. //n It is one of the Islamic works that is most studied and taught throughout the world. In fact, many of the scholars recommend the students of knowledge to begin their path of learning by studying and mastering this booklet. The reason for this is because it was written by the author in a simple manner and comprising of the basic fundamentals of this Religion in brief. The material contained in the treatise was designed to be easily memorized and understood.",
+      "Thalaathatul-Usool (The Three Fundamental Principles) is one of the famous works of al Imaam Muhammad bin ‘Abdil-Wahhaab (may Allah have mercy on him). Without doubt The three fundamental principles of islam are amongst the most important matters that every person will be asked about; ‘Who is your Lord?’, ‘What is your religion?’ and ‘Who is your Prophet?’. So its obligatory upon mankind to know its proofs and making it a study until we reach our next abode, the Grave. \n It is one of the Islamic works that is most studied and taught throughout the world. In fact, many of the scholars recommend the students of knowledge to begin their path of learning by studying and mastering this booklet. The reason for this is because it was written by the author in a simple manner and comprising of the basic fundamentals of this Religion in brief. The material contained in the treatise was designed to be easily memorized and understood.",
     bookUri:
       "https://abdurrahmanorg.files.wordpress.com/2014/09/arabic-text-of-al-usool-ath-thalaathah.pdf",
     audio: {
@@ -63,7 +63,7 @@ export const MUTOONS = [
     id: "58694a0f-3da1-471f-bd96-145571e29d74",
     title: "Nawaaqid-l-Islam",
     author: "Muhammad bn Abdulwahab",
-    image: require("./assets/kitabtawhid.png"),
+    image: require("./assets/ten-nullifiers.png"),
     summary:
       "Nawaqid Al Islaam (The Nullifiers) – Matn (Arabic and English Text) & Arabic Audio",
     bookUri:
@@ -78,11 +78,11 @@ export const MUTOONS = [
     id: "58694a0f-3da1-471f-bd97-145571e29d74",
     title: "Qawaaid-l-Arba'a",
     author: "Muhammad bn Abdulwahab",
-    image: require("./assets/sharhsunnah.png"),
+    image: require("./assets/4-principles.png"),
     summary:
-      "Qawaaid-l-Arba'a (The Four Principles) – Matn (Arabic and English Text) & Arabic Audio",
+      "This is the book: The Four Principles which Shaikh Al-Islaam Muhammad Ibn Abdil-Wahhaab has written. Though it is a short treatise, it emulates The Three Fundamental Principles from the point of view that it is necessary for the students of knowledge to grasp it.\n Before you is a tremendous treatise that was written by Shaikh Al-Islam Muhammad Ibn ’Abdul Wahh’ (d.1206AH). He was a great Scholar and Reviver (Mujaddid) of Islam of his era. The Four Principles that he expounds upon are related to Tawheed and Shirk. Tawheed refers to the fact that there is none who has the right to be worshipped except Allah alone, and that is the purpose behind the creation of humankind and Jinn as well as the primary purpose behind the sending of the Prophets and Messengers. Shirk is the opposite of Tawheed and its nullifier. It is to direct worship to others, other than Allah. The author states the grievous sin of Shirk and how many Muslims in later times fell into polytheistic practices such as supplicating to the deceased in their graves and depending upon them for salvation. The principles are not based upon mere personal deductions, rather they are taken from the Qur’an and the Sunnah of the Prophet (s).",
     bookUri:
-      "https://abdurrahmanorg.files.wordpress.com/2014/09/arabic-matn-of-nawaaqid-al-islaam.pdf",
+      "http://www.salafipublications.com/sps/downloads/pdf/TAW070009.pdf",
     audio: {
       uri: "https://archive.org/download/Mutun_Sawtiya/003-Qawaid_4.mp3",
       reciter: "Abu Ahmad shaydhami",
@@ -93,9 +93,9 @@ export const MUTOONS = [
     id: "58694a0f-3db1-471f-bd96-145571e29d74",
     title: "Al-Arbaun Nawawi",
     author: "Imam An-Nawawi",
-    image: require("./assets/usuulsunnah.png"),
+    image: require("./assets/40-hadith.png"),
     summary:
-      "Nawaqid Al Islaam (The Nullifiers) – Matn (Arabic and English Text) & Arabic Audio",
+      "The book, “Forty Hadiths” by the great scholar and specialist in many fields of knowledge. Al-Imam Yahya bin Sharaf An-Nawawi may Allah shower blessings on him — contains a collection of narrations with simple expressions entailing comprehensive meanings from the Messenger of Allah (Peace and blessings be upon him), generally called Ahadith al-Jawami’.\n Al-Imam An-Nawawi may Allah shower blessings on him so carefully selected authentic narrations to widely cover the issues of pillars of Islam and Eeman, excellent manners, good conduct and other noble deeds and some fundamentals of Adjudication. And by Allah’s Will, the book has had great positive impacts and found acceptance amongst the Muslims generation after generation. Allah the Almighty Alone knows the number of commentaries and explanations of “Forty Hadith by an An-Nawaw that has been given by the people of knowledge and their students over time.\n ",
     bookUri:
       "https://abdurrahmanorg.files.wordpress.com/2014/09/arabic-matn-of-nawaaqid-al-islaam.pdf",
     audio: {
@@ -103,5 +103,65 @@ export const MUTOONS = [
       reciter: "Abu Ahmad shaydhami",
     },
     playlist: ["Mutoon Taalib-l-Ilm 1", "Hadith"],
+  },
+  {
+    id: "58694a0f-3db1-471f-bd96-145576e29d74",
+    title: "Muqaddimah Al-Ajrumiyyah",
+    author: "Imam Ibn Ajrum",
+    image: require("./assets/ajrumiyyah.png"),
+    summary:
+      "al-Ājurrūmiyyah (Arabic: الْآجُرُّومِيَّةِ) in full Al-Muqaddimah al-Ajurrumiyyah fi Mabadi’ Ilm al-Arabiyyah is a 13th-century book of Arabic grammar (نحو عربي naḥw ʿarabī). Very concise for easy memorization, it formed the foundation of a beginner's education in Classical Arabic learning in Arab societies.\n The text itself although not named as such by the author, has become known as ‘al-Ajrumiyyah. The Ajrumiyyah, is the quintessence of Arabic grammar, its status is largely unchallenged as an excellent introduction to this first field of learning, which every scholar must master before delving into other Arabic literature. We find much attention has been paid to it amongst Arab scholars over a considerable period of time.",
+    bookUri:
+      "https://abdurrahmanorg.files.wordpress.com/2014/09/arabic-matn-of-nawaaqid-al-islaam.pdf",
+    audio: {
+      uri: "https://archive.org/download/Mutun_Sawtiya/085-Ajroumiah.mp3",
+      reciter: "Abu Ahmad shaydhami",
+    },
+    playlist: ["Mutoon Taalib-l-Ilm 3", "Arabic"],
+  },
+  {
+    id: "58694a0f-3db1-471f-bd96-145571e79d74",
+    title: "Al-Aqeedah Wasitiyyah",
+    author: "Imam Ibn Taymiyyah",
+    image: require("./assets/sharh-al-aqida-at-wasitiyyah.jpg"),
+    summary:
+      "Matn al-Aqidatul Wasitiya-Sheikh al-Islam Ibn Taymiyyah, who is among the most important treaties of Aqeedah and most essential, that although supporting above all faith in the names and attributes of Allah, also establishes many other points in the Aqeedah of Ahlus Sunnah.ed from several manuscripts, authenticated",
+    bookUri:
+      "https://d1.islamhouse.com/data/ar/ih_books/single_010/ar-alaqidah-alwasetiyaa.pdf",
+    audio: {
+      uri: "https://archive.org/download/Mutun_Sawtiya/041-Alwassitiah.mp3",
+      reciter: "Abu Ahmad shaydhami",
+    },
+    playlist: ["Mutoon Taalib-l-Ilm 3"],
+  },
+  {
+    id: "58694a0f-3db1-471f-bd96-145571e79d74",
+    title: "Matn al-Binaa",
+    author: "Imam Ibn Taymiyyah",
+    image: require("./assets/matn-binaa.png"),
+    summary:
+      "Nawaqid Al Islaam (The Nullifiers) – Matn (Arabic and English Text) & Arabic Audio",
+    bookUri:
+      "https://abdurrahmanorg.files.wordpress.com/2014/09/arabic-matn-of-nawaaqid-al-islaam.pdf",
+    audio: {
+      uri: "https://archive.org/download/Mutun_Sawtiya/087-Matn_binaa_al-afaaal.mp3",
+      reciter: "Abu Ahmad shaydhami",
+    },
+    playlist: ["Arabic"],
+  },
+  {
+    id: "58694a0f-3db1-471f-bd96-145571e79d74",
+    title: "Lamiyyat-ul-Arab",
+    author: "Imam Ibn Taymiyyah",
+    image: require("./assets/usuulsunnah.png"),
+    summary:
+      "Nawaqid Al Islaam (The Nullifiers) – Matn (Arabic and English Text) & Arabic Audio",
+    bookUri:
+      "https://abdurrahmanorg.files.wordpress.com/2014/09/arabic-matn-of-nawaaqid-al-islaam.pdf",
+    audio: {
+      uri: "https://archive.org/download/Mutun_Sawtiya/088-Molhat_al-i3raab.mp3",
+      reciter: "Abu Ahmad shaydhami",
+    },
+    playlist: ["Arabic"],
   },
 ];
